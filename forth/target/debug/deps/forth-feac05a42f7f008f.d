c538c0e4166d592d74c0114f3dba57c806c20998
mazdak <nazemi.works@gmail.com>
2021-02-24 23:50:49 +0400

    more on

---
+++ forth/target/debug/deps/forth-feac05a42f7f008f.d
@@ -0,0 +1,5 @@
+/home/mkz/Desktop/rust.go/forth/target/debug/deps/forth-feac05a42f7f008f: src/main.rs
+
+/home/mkz/Desktop/rust.go/forth/target/debug/deps/forth-feac05a42f7f008f.d: src/main.rs
+
+src/main.rs:
---
+++ forth/target/debug/forth.d
@@ -0,0 +1,1 @@
+/home/mkz/Desktop/rust.go/forth/target/debug/forth: /home/mkz/Desktop/rust.go/forth/src/main.rs
